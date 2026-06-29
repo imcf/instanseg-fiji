5da218b83287bd144e49b40f88f4ce5c6191f22e
Rohan Girish <r.rohangirish@unibas.ch>
2026-06-29 14:34:19 +0200

    Change env installation to %APPDATA% on Windows

--- install.bat
+++ install.bat
@@ -1,8 +1,12 @@
 @echo off
-:: Install the InstanSeg pixi environment.
-:: Run this once from the InstanSeg plugin folder before using the Fiji plugin.
+:: Install the InstanSeg pixi environment into %APPDATA%\InstanSeg so that
+:: Fiji's script discovery never sees the environment files.
 
-pushd "%~dp0"
+set "INSTALL_DIR=%APPDATA%\InstanSeg"
+
+echo InstanSeg environment will be installed to:
+echo   %INSTALL_DIR%
+echo.
 
 where pixi >nul 2>&1
 if errorlevel 1 (
@@ -12,8 +16,19 @@ if errorlevel 1 (
     exit /b 1
 )
 
+if not exist "%INSTALL_DIR%" mkdir "%INSTALL_DIR%"
+
+echo Copying environment files...
+copy /Y "%~dp0pixi.toml" "%INSTALL_DIR%\pixi.toml" >nul
+if exist "%~dp0pixi.lock" copy /Y "%~dp0pixi.lock" "%INSTALL_DIR%\pixi.lock" >nul
+
+pushd "%INSTALL_DIR%"
 echo Installing InstanSeg environment (this may take a few minutes)...
 pixi install
+popd
+
 echo.
-echo Done. You can now run the InstanSeg plugin in Fiji.
+echo Done. Python environment is at:
+echo   %INSTALL_DIR%\.pixi\envs\default
+echo You can now run the InstanSeg plugin in Fiji.
 pause
